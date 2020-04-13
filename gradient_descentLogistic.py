46beaf006815d6627a1640ef22b8b446db2207c5
Stephen491 <60985179+Stephen491@users.noreply.github.com>
2020-04-13 02:25:54 -0400

    Update gradient_descentLogistic.py

--- gradient_descentLogistic.py
+++ gradient_descentLogistic.py
@@ -6,5 +6,4 @@ def gradient_descent(x,y,theta,alpha,lambda1, iterations):
     m =  np.size(x,0)
     for i in range(iterations):
         theta = theta - alpha*gradient(x,y,theta,lambda1)
-        #print(getCost(x, y, theta, lambda1))
     return theta
